20716b50cf6de7ab9146af2f7219a41b787b1c1d
caoxuejun2 <1831482142@qq.com>
2020-11-09 20:17:10 +0800

    处理了rem问题

--- src/views/SearchResult/index.vue
+++ src/views/SearchResult/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id='app'>
-    <router-view></router-view>
+  <div class="page-search-result">
+    <h1>搜索结果页面</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'SearchResult'
 }
 </script>
 
